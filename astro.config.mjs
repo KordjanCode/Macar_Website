31b96f9803f36d5e5ac71519cdbe64b996d0def3
KordjanCode <korecki01@gmail.com>
2024-10-15 09:48:01 +0200

    Changes in the astro.config.mjs_2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://github.com/KordjanCode', // Replace with your GitHub Pages URL
-    base: '', // Replace with your repository name, or leave blank if deploying to a root domain
+    base: '/Macar_Website/', // Replace with your repository name, or leave blank if deploying to a root domain
   });
-
-  // site: 'https://github.com/KordjanCode', // Replace with your GitHub Pages URL
-  // base: '/Macar_Website/', // Replace with your repository name, or leave blank if deploying to a root domain
