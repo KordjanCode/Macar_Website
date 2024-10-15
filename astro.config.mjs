e59253ff2f9d95aa14746f8fdc50e27506fd1d2d
KordjanCode <korecki01@gmail.com>
2024-10-15 12:05:54 +0200

    Removed the base from config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://github.com/KordjanCode', // Replace with your GitHub Pages URL
-    base: '/Macar_Website/', // Replace with your repository name, or leave blank if deploying to a root domain
+    base: '', // Replace with your repository name, or leave blank if deploying to a root domain
   });
