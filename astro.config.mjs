15b2e426c80cb5f5592284c932f40e8de88d7cf8
KordjanCode <korecki01@gmail.com>
2024-10-15 12:12:39 +0200

    Quick Fix in index.html

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,3 +6,5 @@ export default defineConfig({
     site: 'https://github.com/KordjanCode', // Replace with your GitHub Pages URL
     base: '', // Replace with your repository name, or leave blank if deploying to a root domain
   });
+
+  // base: '/Macar_Website/'
